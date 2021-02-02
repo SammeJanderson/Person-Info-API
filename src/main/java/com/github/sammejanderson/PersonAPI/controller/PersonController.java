ff382fff8e865b41b7d2063cac02b88b50341378
SammeJanderson <samme.san@gmail.com>
2021-02-02 15:58:10 -0300

    more tests

---
+++ src/main/java/com/github/sammejanderson/PersonAPI/controller/PersonController.java
@@ -0,0 +1,9 @@
+package com.github.sammejanderson.PersonAPI.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi/person")
+public class PersonController {
+}
---
+++ src/main/java/com/github/sammejanderson/PersonAPI/service/PersonService.java
@@ -0,0 +1,6 @@
+/** esse serviço converte a entidade para DTO para que possa ser acessado com segurança*/
+
+package com.github.sammejanderson.PersonAPI.service;
+
+public class PersonService {
+}
